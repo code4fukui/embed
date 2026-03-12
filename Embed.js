ba9569014450bb0aba1ee49da497135c6e1d099f
Taisuke Fukuno <fukuno@jig.jp>
2026-03-12 13:56:32 +0900

    support a format IEEE32 in base64

--- Embed.js
+++ Embed.js
@@ -2,11 +2,20 @@ import { getEmbed } from "./getEmbed.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import { normalizeVector } from "./normalizeVector.js";
 import { DotScores } from "https://code4fukui.github.io/dotscores-simd/DotScores.js";
+import { IEEE32 } from "https://code4fukui.github.io/IEEE754/IEEE32.js";
+import { Base64URL } from "https://code4fukui.github.io/Base64URL/Base64URL.js";
+
+const decode = (vec) => {
+  if (vec.indexOf(",") >= 0) {
+    return JSON.parse(`[${vec}]`);
+  }
+  return IEEE32.decode(Base64URL.decode(vec));
+};
 
 export class Embed {
   static async create(fncsv) {
     const entries = await CSV.fetchJSON(fncsv);
-    const vec = entries.map(i => JSON.parse(`[${i.vec}]`));
+    const vec = entries.map(i => decode(i.vec));
     const dots = await DotScores.create(vec);
     return new Embed(dots, entries);
   }
--- addembed.js
+++ addembed.js
@@ -1,14 +1,28 @@
 import { getEmbed } from "./getEmbed.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import { IEEE32 } from "https://code4fukui.github.io/IEEE754/IEEE32.js";
+import { Base64URL } from "https://code4fukui.github.io/Base64URL/Base64URL.js";
 
 const fn = Deno.args[0] || "./data.txt";
 const data = (await Deno.readTextFile(fn)).split("\n");
+
+const format = Deno.args[1] || "base64"; // "json";
+
+const convert = (format, array) => {
+  if (format == "json") {
+    return array.join(",");
+  } else if (format == "base64") {
+    return Base64URL.encode(IEEE32.encode(array));
+  } else {
+    throw new Error("unknown format: " + format);
+  }
+};
 
 const list = [];
 for (const item of data) {
   if (!item) continue;
   const res = await getEmbed(item);
   console.log(item, res);
-  list.push({ text: item, vec: res.join(",") });
+  list.push({ text: item, vec: convert(format, res) });
   await Deno.writeTextFile("data.csv", CSV.stringify(list));
 }
